6666bfb4c9b80d3b9a838ef58043d092023ecb01
Aliaksandr Valialkin <valyala@gmail.com>
2016-09-13 18:43:50 +0300

    an attempt to improve godoc formatting

--- tcplisten.go
+++ tcplisten.go
@@ -3,14 +3,14 @@
 // Package tcplisten provides customizable TCP net.Listener with various
 // performance-related options:
 //
-// - SO_REUSEPORT. This option allows linear scaling server performance
-//   on multi-CPU servers.
-//   See https://www.nginx.com/blog/socket-sharding-nginx-release-1-9-1/ for details.
+//   - SO_REUSEPORT. This option allows linear scaling server performance
+//     on multi-CPU servers.
+//     See https://www.nginx.com/blog/socket-sharding-nginx-release-1-9-1/ for details.
 //
-// - TCP_DEFER_ACCEPT. This option expects the server reads from the accepted
-//   connection before writing to them.
+//   - TCP_DEFER_ACCEPT. This option expects the server reads from the accepted
+//     connection before writing to them.
 //
-// - TCP_FASTOPEN. See https://lwn.net/Articles/508865/ for details.
+//   - TCP_FASTOPEN. See https://lwn.net/Articles/508865/ for details.
 //
 // The package is derived from https://github.com/kavu/go_reuseport .
 package tcplisten
